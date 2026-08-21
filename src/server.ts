3693b1e03a17a66439a11ce3021ec03003730b10
Ritwik <ritwik.b2001@gmail.com>
2026-08-21 23:22:57 +0530

    feat: update server setup and add express app
    - Changed the dev script to use nodemon for automatic server restarts.
    - Added express as a dependency and created a new app module.
    - Configured dotenv to load environment variables, including NODE_ENV.
    - Refactored server initialization to handle errors gracefully.
    - Enabled esModuleInterop in TypeScript configuration for better module compatibility.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from './app'
 import { Config } from './config'
 
-console.log('Server is running on port ', Config.PORT)
+const startServer = () => {
+    const PORT = Config.PORT
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.error('Error starting server:', error)
+        process.exit(1)
+    }
+}
+
+startServer()
