cbb074b51d8dff57b1ec2d7fa767714012761c4e
Ritwik <ritwik.b2001@gmail.com>
2026-08-22 00:12:12 +0530

    feat: add winston logger for improved logging and error handling

---
+++ src/config/logger.ts
@@ -0,0 +1,39 @@
+import winston from 'winston'
+import { Config } from '.'
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: { service: 'auth-service' },
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: false, // Set to true to disable logging to this file
+        }),
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: false, // Set to true to disable logging to this file
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            silent: Config.NODE_ENV === 'test', // Set to true to disable logging to the console
+        }),
+    ],
+})
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import app from './app'
 import { Config } from './config'
+import logger from './config/logger'
 
 const startServer = () => {
     const PORT = Config.PORT
     try {
         app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`)
+            logger.error('test error')
+            logger.info('Server is running on port', { port: PORT })
         })
     } catch (error) {
-        console.error('Error starting server:', error)
+        logger.error('Error starting server:', { error })
         process.exit(1)
     }
 }
